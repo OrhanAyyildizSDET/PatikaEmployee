b1d381a137f4c771d8f47123ea1654e62aad4de2
Orhan Ayyıldız <ayyildizorhann03@gmail.com>
2022-11-12 12:53:38 +0300

    Hypotenus

---
+++ src/main/java/Hipotenüs.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Hipotenüs {
+    static Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+        System.out.println("Please enter the first line of triangular");
+        int a = scanner.nextInt();
+        System.out.println("Please enter the second line of triangular");
+        int b = scanner.nextInt();
+        double c = Math.sqrt(a * a + b * b);
+        System.out.println("Hypotenuse is : "+String.format("%.3f",c));
+    }
+}
