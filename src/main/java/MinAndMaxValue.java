f5d3b4e1bf7e78c7f5fa8402c785c16a69dd58bc
Orhan Ayyıldız <ayyildizorhann03@gmail.com>
2022-11-17 12:57:52 +0300

    sort

---
+++ src/main/java/MinAndMaxValue.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Scanner;
+
+public class MinAndMaxValue {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("How many number do you want to enter?");
+        int count = scanner.nextInt();
+        int[] numbers = new int[count];
+
+        int counter = 0;
+        int a = 0;
+        while (a<count){
+            System.out.print("Enter a number "+counter+" : ");
+            int number = scanner.nextInt();
+            numbers[a] = number;
+            a++;
+        }
+
+        Arrays.sort(numbers);
+        System.out.println("The biggest number is "+numbers[numbers.length-1]);
+        System.out.println("The smallest number is "+numbers[0]);
+    }
+}
