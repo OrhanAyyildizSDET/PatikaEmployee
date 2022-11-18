d9afe5b2c83ad9ce4f5dc9dea30470d6cb0ab843
Orhan Ayyıldız <ayyildizorhann03@gmail.com>
2022-11-18 12:27:12 +0300

    powerRecursice

---
+++ src/main/java/powerRecursive.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class powerRecursive {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        while (true){
+            System.out.print("Please enter the number  ");
+            int a = scanner.nextInt();
+            System.out.print("Please enter the power  ");
+            int b = scanner.nextInt();
+            int result = 1;
+            System.out.println(a+" power "+b+" is "+power(a,b,result));
+
+            System.out.println("Do you want to continue with new values?  (yes)  (no)");
+            String value = scanner.next().trim().toLowerCase();
+            if(!value.equals("yes")){
+                break;
+            }
+        }
+
+    }
+    static int power(int a, int b,int result) {
+        if (a != 0){
+            if (b == 0) {
+                return result;
+            }
+            else {
+              result *=a ;
+              b--;
+              return power(a,b,result);
+            }
+        }
+        else {
+            System.out.println(a+" power "+b+ " = 0");
+        }
+        return result;
+    }
+}
