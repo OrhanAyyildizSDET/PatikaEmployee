0c2ab0dbcf144a0ef388ddfcf2148ffaaa9e7526
Orhan Ayyıldız <ayyildizorhann03@gmail.com>
2022-11-22 11:15:02 +0300

    last

--- src/main/java/Patika1_2Week/MinAndMaxValue.java
+++ src/main/java/Patika1_2Week/MinAndMaxValue.java
@@ -24,5 +24,7 @@ public class MinAndMaxValue {
         Arrays.sort(numbers);
         System.out.println("The biggest number is "+numbers[numbers.length-1]);
         System.out.println("The smallest number is "+numbers[0]);
+        System.out.println("The second biggest number is "+numbers[numbers.length-2]);
+        System.out.println("The second smallest number is "+numbers[1]);
     }
 }
